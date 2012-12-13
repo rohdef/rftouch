cc4697879bf140f6695b88a27eaec26a40b255ea
Rohde Fischer <rohdef@rohdef.dk>
2012-12-13 12:41:30 +0100

    Reload store events reflect what they expect

--- coffeescript/test/data/ReloadStore-test.coffee
+++ coffeescript/test/data/ReloadStore-test.coffee
@@ -10,11 +10,45 @@ buster.testCase "ReloadStore", {
       proxy: this.testFailProxy
     }
   
-  "ReloadStore fails on a single try": (done) ->
+  "ReloadStore can succeed in first try": (done) ->
+    this.testFailProxy.setFailCount(0)
     this.reloadStore.on 'load', (store, records, successful, operation, opts) ->
-      expect(successful).toEqual(false)
-      
+      expect(successful).toEqual(true)
       done()
     
     this.reloadStore.load()
+  
+  "Bailout event is fired after 3 tries": (done) ->
+    tries = 0
+    
+    this.reloadStore.on 'load', (store, records, successful, operation, opts) ->
+      tries += 1
+    
+    this.reloadStore.on 'bailout', (store) ->
+      expect(tries).toEqual(3)
+      done()
+  
+  "Retrying on bailout will cause 3 new retries": (done) ->
+    tries = 0
+    
+    this.testFailProxy.setFailCount(6)
+    this.reloadStore.on 'load', (store, records, successful, operation, opts) ->
+      tries += 1
+    
+    this.reloadStore.on 'bailout', (store) ->
+      if (tries == 3)
+        store.load()
+      else
+        expect(tries).toEqual(6)
+        done()
+  
+  "Store succeeds after 2 reload when failproxy is set to fail twice": (done) ->
+    this.testFailProxy.setFailCount(2)
+    done()
+  
+  "Store succeeds when retrying on bailout": (done) ->
+    done()
+  
+  "Bailout event is fired every time when retries is set to 0": (done) ->
+    done()
 }
--- test/data/ReloadStore-test.js
+++ test/data/ReloadStore-test.js
@@ -11,11 +11,49 @@ buster.testCase("ReloadStore", {
       proxy: this.testFailProxy
     });
   },
-  "ReloadStore fails on a single try": function(done) {
+  "ReloadStore can succeed in first try": function(done) {
+    this.testFailProxy.setFailCount(0);
     this.reloadStore.on('load', function(store, records, successful, operation, opts) {
-      expect(successful).toEqual(false);
+      expect(successful).toEqual(true);
       return done();
     });
     return this.reloadStore.load();
+  },
+  "Bailout event is fired after 3 tries": function(done) {
+    var tries;
+    tries = 0;
+    this.reloadStore.on('load', function(store, records, successful, operation, opts) {
+      return tries += 1;
+    });
+    return this.reloadStore.on('bailout', function(store) {
+      expect(tries).toEqual(3);
+      return done();
+    });
+  },
+  "Retrying on bailout will cause 3 new retries": function(done) {
+    var tries;
+    tries = 0;
+    this.testFailProxy.setFailCount(6);
+    this.reloadStore.on('load', function(store, records, successful, operation, opts) {
+      return tries += 1;
+    });
+    return this.reloadStore.on('bailout', function(store) {
+      if (tries === 3) {
+        return store.load();
+      } else {
+        expect(tries).toEqual(6);
+        return done();
+      }
+    });
+  },
+  "Store succeeds after 2 reload when failproxy is set to fail twice": function(done) {
+    this.testFailProxy.setFailCount(2);
+    return done();
+  },
+  "Store succeeds when retrying on bailout": function(done) {
+    return done();
+  },
+  "Bailout event is fired every time when retries is set to 0": function(done) {
+    return done();
   }
 });
